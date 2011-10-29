fc0cdf239ff107b3b31d8b504ef0a78089cc8607
Sarah Hoffmann <lonvia@denofr.de>
2011-10-29 17:28:33 +0200

    fix copyright notices

--- osgende/common/postgisconn.py
+++ osgende/common/postgisconn.py
@@ -1,5 +1,5 @@
-# This file is part of Lonvia's Hiking Map
-# Copyright (C) 2010 Sarah Hoffmann
+# This file is part of Osgende
+# Copyright (C) 2011 Sarah Hoffmann
 #
 # This is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
