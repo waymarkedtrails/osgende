93fdd98a8d02a55a75c777497e1f9b835bf3bb55
Michael <code@datendelphin.net>
2013-01-21 21:27:38 +0100

    appropriate comments, added layout function

--- osgende/ways/ways.py
+++ osgende/ways/ways.py
@@ -14,14 +14,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+"""
+Tables for ways
+"""
 
 from osgende.subtable import OsmosisSubTable
 from osgende.tags import TagStore
 
 class Ways(OsmosisSubTable):
-    """A relation collection class that gets updated according to
-       the changes in a RelationSegment table. If an optional hierarchy
-       table is provided, super relations will be updated as well.
+    """Most basic table type to construct a simple derived table from
+       the ways table. The extension to OsmosisSubTable is that
+       it constructs the geometry of the way.
+       Use 'geom' to state the name of the column that should contain
+       the geometry.
     """
 
     # Name of predefined columns
@@ -32,10 +37,20 @@ class Ways(OsmosisSubTable):
     def __init__(self, db, name, subset):
         OsmosisSubTable.__init__(self, db, 'way', name, subset)
 
+    def layout(self, columns):
+        """ Layout the table as specified in PGTable.layout() but
+            it will add a column for the OSM id. The name of the column
+            is specified in 'column_id'.
+        """
+        OsmosisSubTable.layout(self, columns)
+        self.add_geometry_column(self.column_geom, self.srid,
+                                 'GEOMETRY', with_index=True)
+
     def _process_next(self, obj):
         tags = self.transform_tags(obj['id'], TagStore(obj['tags']))
 
         if tags is not None:
+            # FIXME: does not work for nodestore
             geom = """ST_Transform(ST_MakeLine(ARRAY(
                       SELECT geom FROM (SELECT generate_subscripts(nodes, 1) AS i,
                           nodes FROM public.ways WHERE id = %d) w,
