3f0edb954d5dee3fd7d31e5cb26c3377e1700862
Sarah Hoffmann <lonvia@denofr.de>
2017-11-05 21:19:33 +0100

    move tags into common folder

--- osgende/subtable.py
+++ osgende/subtable.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from sqlalchemy import Table, Column, BigInteger, select, and_
-from osgende.tags import TagStore
+from osgende.common.tags import TagStore
 from osgende.common.connectors import TableSource
 from osgende.common.threads import ThreadableDBObject
 
