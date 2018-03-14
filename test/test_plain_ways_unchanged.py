77c0fd281264aa5587166bfcfa3cd547fa87961a
Sarah Hoffmann <lonvia@denofr.de>
2018-03-14 23:02:36 +0100

    add updates for plain way table

--- test/test_plain_ways_unchanged.py
+++ test/test_plain_ways_unchanged.py
@@ -26,15 +26,67 @@ from table_test_fixture import TableTestFixture
 
 class TestPlainWayTableUnchanged(TableTestFixture):
 
-    nodes = { 1: (1.0, 2.1), 2: (1.0, 2.2) }
+    nodes = { 1: (1.0, 2.1), 2: (1.0, 2.2), 3: (1.0, 2.3),
+              34: (0.9, 2.1), 36: (1.2, 2.5),
+              101: (4.5, -34.1), 103: (4.51, -34.111)}
+
+    baseimport = """\
+                   w101 Tname=first Nn1,n2
+                   w103 Tname=second Nn34,n1,n36
+                 """
+
+    expect_w101 = { 'id' : 101, 'tags' : { 'name' : 'first' },
+                    'nodes' : [1, 2], 'geom' : Line(1, 2) }
+    expect_w103 = { 'id' : 103, 'tags' : { 'name' : 'second' },
+                    'nodes' : [34, 1, 36], 'geom' : Line(34, 1, 36) }
 
     def create_tables(self, db):
         return [PlainWayTable(db.metadata, "test", db.osmdata.way, db.osmdata)]
 
     def test_create(self):
-        self.import_data("""\
-                w101 Tname=first Nn1,n2
-                """, self.nodes)
-        self.table_equals("test", [
+        self.import_data(self.baseimport, self.nodes)
+        self.table_equals("test", [self.expect_w101, self.expect_w103])
+
+    def test_update_add_way(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""w3 Tx=y Nn101,n103""")
+        self.has_changes("test_changeset", ['A3'])
+        self.table_equals("test", [self.expect_w101, self.expect_w103,
+            {'id': 3, 'tags': { 'x': 'y'}, 'nodes': [101, 103], 'geom': Line(101, 103)}
+            ])
+
+    def test_update_delete_way(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""w101 v2 dD""")
+        self.has_changes("test_changeset", ['D101'])
+        self.table_equals("test", [self.expect_w103])
+
+    def test_update_change_tags(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""w101 v2 Tname=new Nn1,n2""")
+        self.has_changes("test_changeset", ['M101'])
+        new_w101 = dict(self.expect_w101)
+        new_w101.update({'tags' : {"name" : "new"}})
+        self.table_equals("test", [new_w101, self.expect_w103])
+
+    def test_update_add_node(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""w101 v2 Tname=first Nn1,n2,n3""")
+        self.has_changes("test_changeset", ['M101'])
+        self.table_equals("test", [self.expect_w103,
             { 'id' : 101, 'tags' : { 'name' : 'first' },
-              'nodes' : [1, 2], 'geom' : Line(1, 2) }])
+                    'nodes' : [1, 2, 3], 'geom' : Line(1, 2, 3) }])
+
+    def test_delete_node_invalidate_way(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""w101 v2 Tname=first Nn1""")
+        self.has_changes("test_changeset", ['D101'])
+        self.table_equals("test", [self.expect_w103])
+
+    def test_move_node(self):
+        self.import_data(self.baseimport, self.nodes)
+        self.update_data("""n2 v2 x0.9 y2.1""")
+        self.has_changes("test_changeset", ['M101'])
+        self.table_equals("test", [self.expect_w103,
+            { 'id' : 101, 'tags' : { 'name' : 'first' },
+                    'nodes' : [1, 2], 'geom' : Line(1, (0.9, 2.1)) }])
