c7d46b25f95831febf8cf74a7e82196bb224e076
Sarah Hoffmann <lonvia@denofr.de>
2018-10-17 21:40:54 +0200

    add updates for grouped way

--- osgende/lines/grouped.py
+++ osgende/lines/grouped.py
@@ -17,7 +17,7 @@
 
 from osgende.common.table import TableSource
 from osgende.common.sqlalchemy import DropIndexIfExists
-from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.dialects.postgresql import ARRAY, insert
 
 import sqlalchemy as sa
 
@@ -33,12 +33,18 @@ class GroupedWayTable(TableSource):
     def __init__(self, meta, name, source, rows):
         table = sa.Table(name, meta,
                          sa.Column('id', sa.BigInteger),
-                         sa.Column('child', source.c.id.type, index=True))
+                         sa.Column('child', source.c.id.type, unique=True, index=True))
 
-        super().__init__(table)
+        super().__init__(table, name + "_changeset")
 
         self.rows = rows
         self.src = source
+
+    def _select_src(self):
+        rows = [self.src.c[r] for r in self.rows]
+        rows.extend((self.src.c.id, self.src.c.nodes))
+
+        return sa.select(rows)
 
     def construct(self, engine):
         """ Create full table content from the source table.
@@ -46,33 +52,70 @@ class GroupedWayTable(TableSource):
         self.truncate(engine)
 
         done = set()
-        to_select = [self.src.c[r] for r in self.rows]
-        to_select.extend((self.src.c.id, self.src.c.nodes))
-        cur = engine.execution_options(stream_results=True).execute(sa.select(to_select))
+        with engine.begin() as conn:
+            cur = conn.execution_options(stream_results=True).execute(self._select_src())
 
-        for obj in cur:
-            oid = obj['id']
-            if oid in done:
-                # already has an id
-                continue
+            for obj in cur:
+                oid = obj['id']
+                if oid in done:
+                    # already has an id
+                    continue
 
-            properties = [obj[name] for name in self.rows]
+                done.update(self._insert_adjacent_ways((oid, obj['nodes']), obj,
+                                                       conn))
 
-            merge_list = self._get_all_adjacent_way_ids(
-                           (oid, obj['nodes']), properties, engine)
 
-            # only insert ways that have neighbours
-            if len(merge_list) > 1:
-                engine.execute(self.data.insert(),
-                               [ { 'id' : oid, 'child' : x } for x in merge_list ])
-                done.update(merge_list)
+    def update(self, engine):
+        with engine.begin() as conn:
+            changes = {}
+            todo = set()
+            done = set()
+            # Remove all virtual IDs that are directly affected by the change.
+            modsql = sa.select([self.c.id], distinct=True)\
+                         .where(self.c.child.in_(self.src.select_modify_delete())).alias()
 
-    def _get_all_adjacent_way_ids(self, base, properties, conn):
+            delsql = self.data.delete().where(self.c.id.in_(modsql))
+
+            for row in conn.execute(delsql.returning(self.c.id, self.c.child)):
+                changes[row[0]] = 'D'
+                todo.add(row[1])
+
+            # Insert added ways
+            addsql = self._select_src()\
+                       .where(self.src.c.id.in_(self.src.select_add_modify()))
+            cur = engine.execution_options(stream_results=True).execute(addsql)
+
+            for obj in cur:
+                oid = obj['id']
+                if oid in done:
+                    # already has an id
+                    continue
+
+                done.update(self._insert_adjacent_ways((oid, obj['nodes']), obj,
+                                                       conn, changes))
+
+            # Insert modified ways
+            for oid in todo:
+                if oid in done:
+                    continue
+
+                cur = conn.execute(self._select_src().where(self.src.c.id == oid))
+
+                for obj in cur:
+                    done.update(self._insert_adjacent_ways((oid, obj['nodes']), obj,
+                                                           conn, changes))
+
+            # finally fill the changeset table
+            self.write_change_table(engine, changes)
+
+
+    def _insert_adjacent_ways(self, base, obj, conn, changes=None):
         """ Find all ways adjacent to the one given in baseid iteratively
             (sharing at least one node and all properties)
             It checks for directly adjacent ways and repeats this procedure
             for all found ways until all indirectly adjacent ways are found
         """
+        properties = [obj[name] for name in self.rows]
         unchecked_ways = [base]
         all_adjacent_ways = [base[0]]
         done_ways = set()
@@ -82,9 +125,7 @@ class GroupedWayTable(TableSource):
             done_ways.add(wid)
 
             s = self.src.data
-            to_select = [s.c[r] for r in self.rows]
-            to_select.extend((s.c.id, s.c.nodes))
-            intersecting_ways = sa.select(to_select)\
+            intersecting_ways = self._select_src()\
                                  .where(s.c.nodes.overlap(sa.cast(wnodes, ARRAY(sa.BigInteger))))\
                                  .where(s.c.id.notin_(done_ways))
 
@@ -96,4 +137,32 @@ class GroupedWayTable(TableSource):
                     all_adjacent_ways.append(candidate['id'])
                     unchecked_ways.append((candidate['id'], candidate['nodes']))
 
+        # only insert ways that have neighbours
+        if len(all_adjacent_ways) <= 1:
+            return []
+
+        if changes is not None:
+            # Figure out if there is an existing group or we have even
+            # combined two groups.
+            res = conn.execute(sa.select([self.c.id], distinct=True)
+                                 .where(self.c.child.in_(all_adjacent_ways)))
+            if res.rowcount > 0:
+                base_id = res.fetchone()[0]
+                changes[base_id] = 'M'
+                for i in res:
+                    changes[i[0]] = 'D'
+
+                sql = insert(self.data)\
+                        .on_conflict_do_update(index_elements=[self.c.child],
+                                               set_={'id' : sa.text('EXCLUDED.id')})\
+                        .values([{'id': base_id, 'child': x} for x in all_adjacent_ways])
+
+                conn.execute(sql)
+                return [base_id] + all_adjacent_ways
+
+            changes[base[0]] = 'M' if base[0] in changes else 'A'
+
+
+        conn.execute(self.data.insert(),
+                     [{'id': base[0], 'child': x} for x in all_adjacent_ways])
         return all_adjacent_ways
