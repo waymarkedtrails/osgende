263d7aeceb697cd9271ec250a34b2f490edf6f13
Sarah Hoffmann <lonvia@denofr.de>
2020-11-07 16:02:59 +0100

    refactor geometry building
    Split code into readable chuncks, add tests and fix a couple
    of issues uncovered by the tests.
    
    Reversing is now always done on a complete element, i.e. for
    relations all its segments get reversed as one, not only the
    last segment.

--- osgende/common/build_geometry.py
+++ osgende/common/build_geometry.py
@@ -22,100 +22,194 @@ import sqlalchemy as sa
 from shapely.geometry import LineString, MultiLineString
 from geoalchemy2.shape import to_shape
 
-def _sqr_dist(p1, p2):
+def _sqr_dist(pt1, pt2):
     """ Returns the squared simple distance of two points.
         As we only compare close distances, we neither care about curvature
         nor about square roots.
     """
-    xd = p1[0] - p2[0]
-    yd = p1[1] - p2[1]
-    return xd * xd + yd * yd
+    return (pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2
+
+class _MultiLine:
+    """ A simple representation of a sequence of line strings.
+    """
+
+    def __init__(self, geom=None):
+        self.turn_point = 0
+        if geom is None:
+            self.turn_point = -1
+            self.geom = []
+        elif isinstance(geom, LineString):
+            self.geom = [list(geom.coords)]
+        elif isinstance(geom, MultiLineString):
+            self.geom = [list(g.coords) for g in list(geom.geoms)]
+        else:
+            raise ValueError(f"Bad geometry type '{type(geom)}'.")
+
+    def to_shape(self):
+        """ Return a Shapely line geometry for the multiline. Returns
+            a LineStirng or MultiLineString depending on the number of segments.
+        """
+        if not self.geom:
+            return None
+
+        if len(self.geom) == 1:
+            return LineString(self.geom[0])
+
+        return MultiLineString(self.geom)
+
+    def empty(self):
+        """ Return `true` if the geometry is empty.
+        """
+        return not self.geom
+
+    def first(self):
+        """ Return the first point of the first segment.
+        """
+        assert self.geom
+        return self.geom[0][0]
+
+    def last(self):
+        """ Return the last point of the last segment.
+        """
+        assert self.geom
+        return self.geom[-1][-1]
+
+    def reverse(self, from_seg=0):
+        """ Reverse the entire geometry. """
+        self.geom[from_seg:] = [list(reversed(g)) for g in reversed(self.geom[from_seg:])]
+
+    def add(self, geom):
+        """ Add the given geometry as new segments to the end of the multiline.
+        """
+        self.turn_point = len(self.geom)
+        self.geom.extend(geom.geom)
+
+    def weld(self, geom):
+        """ Adds the geometry by joining the last point of this geometry
+            and the first point of the given one.
+        """
+        self.geom[-1].extend(geom.geom[0][1:])
+        self.geom.extend(geom.geom[1:])
+
+    def weld_reverse(self, geom):
+        """ Reverses the geometry and adds it by joining the last point of this
+            geometry and the first point of the given one.
+        """
+        self.geom[-1].extend(geom.geom[-1][-2::-1])
+        self.geom.extend((list(reversed(g)) for g in geom.geom[-2::-1]))
+
+    def join_at_end(self, geom):
+        """ Try to connect the given geometry at the end, possibly turning
+            it around. Returns `true` if the join was successful.
+        """
+        if geom.first() == self.last():
+            self.weld(geom)
+            self.turn_point = -1
+            return True
+
+        if geom.last() == self.last():
+            self.weld_reverse(geom)
+            self.turn_point = -1
+            return True
+
+        return False
+
+    def reverse_and_join_at_end(self, geom):
+        """ Try to join the geometries by reversing the last segments starting
+            from the current turn point of our own geometry and joining the
+            given geometry at the new end.
+            Returns `true` if the join was successful.
+        """
+        if self.turn_point < 0:
+            return False
+
+        connpt = self.geom[self.turn_point][0]
+
+        if geom.first() == connpt:
+            self.reverse(self.turn_point)
+            self.weld(geom)
+            self.turn_point = -1
+            return True
+
+        if geom.last() == connpt:
+            self.reverse(self.turn_point)
+            self.weld_reverse(geom)
+            self.turn_point = -1
+            return True
+
+        return False
+
+    def add_at_distance(self, geom):
+        """ Add the geometry as new segments such that the distance to the
+            existing end point is minimal.
+        """
+        mdist = _sqr_dist(self.last(), geom.first())
+        dist_to_last = _sqr_dist(self.last(), geom.last())
+        if dist_to_last < mdist:
+            geom.reverse()
+            mdist = dist_to_last
+
+        # For the second way in the relation, we also allow the first
+        # to be turned, if the two ways aren't connected.
+        if self.turn_point == 0:
+            d_first = _sqr_dist(self.first(), geom.first())
+            d_last = _sqr_dist(self.first(), geom.last())
+            if d_first < mdist or d_last < mdist:
+                self.reverse()
+                if d_last < d_first:
+                    geom.reverse()
+
+        self.add(geom)
+
+
+def _get_member_geometries(conn, members, way_table, rel_table):
+    """ Collect geometries for way and relation members from the respective
+        tables.
+    """
+    geoms = {'W' : {}, 'R' : {}}
+
+    for member in filter(lambda m: m['type'] != 'N', members):
+        geoms[member['type']][member['id']] = None
+
+    for kind, t in (('W', way_table), ('R', rel_table)):
+        if geoms[kind]:
+            sql = sa.select([t.c.id, t.c.geom])\
+                    .where(t.c.id.in_(geoms[kind].keys()))\
+                    .where(t.c.geom is not None)
+            for r in conn.execute(sql):
+                geoms[kind][r.id] = _MultiLine(to_shape(r.geom))
+
+    return geoms
+
 
 def build_route_geometry(conn, members, way_table, rel_table):
     """ Create a route geometry from a relation and way table given
         a member list.
     """
-    geoms = { 'W' : {}, 'R' : {} }
-    for m in members:
-        if m['type'] != 'N':
-            geoms[m['type']][m['id']] = None
-    # first get all involved geoemetries
-    for kind, t in (('W', way_table), ('R', rel_table)):
-        if geoms[kind]:
-            sql = sa.select([t.c.id, t.c.geom]).where(t.c.id.in_(geoms[kind].keys()))
-            for r in conn.execute(sql):
-                geoms[kind][r['id']] = to_shape(r['geom'])
+    geoms = _get_member_geometries(conn, members, way_table, rel_table)
 
-    # now put them together
-    is_turnable = False
-    outgeom = []
-    for m in members:
-        t = m['type']
-        # ignore nodes and missing ways and relations
-        if t == 'N' or m['id'] not in geoms[t]:
+    outgeom = _MultiLine()
+
+    for member in filter(lambda m: m['type'] != 'N', members):
+        geom = geoms[member['type']].get(member['id'])
+        if geom is None:
             continue
 
-        # convert this to a tuple of coordinates
-        geom = geoms[t][m['id']]
-        if geom is None:
-            continue 
-        if geom.geom_type == 'MultiLineString':
-            geom = [list(g.coords) for g in list(geom.geoms)]
-        elif geom.geom_type == 'LineString':
-            geom = [list(geom.coords)]
-        else:
-            raise RuntimeError("Bad geometry type '%s' (member type: %s member id: %d" % (geom.geom_type, t, m['id']))
+        if outgeom.empty():
+            outgeom.add(geom)
+            continue
 
-        if outgeom:
-            # try connect with previous geometry at end point
-            if geom[0][0] == outgeom[-1][-1]:
-                outgeom[-1].extend(geom[0][1:])
-                outgeom.extend(geom[1:])
-                is_turnable = False
-                continue
-            if geom[-1][-1] == outgeom[-1][-1]:
-                outgeom[-1].extend(geom[-1][-2::-1])
-                outgeom.extend(geom[-2::-1])
-                is_turnable = False
-                continue
-            if is_turnable:
-                # try to connect with previous geometry at start point
-                if geom[0][0] == outgeom[-1][0]:
-                    outgeom[-1].reverse()
-                    outgeom[-1].extend(geom[0][1:])
-                    outgeom.extend(geom[1:])
-                    is_turnable = False
-                    continue
-                if geom[-1] == outgeom[-1][0]:
-                    outgeom[-1].reverse()
-                    outgeom[-1].extend(geom[-1][-2::-1])
-                    outgeom.extend(geom[-2::-1])
-                    is_turnable = False
-                    continue
-            # nothing found, then turn the geometry such that the
-            # end points are as close together as possible
-            mdist = _sqr_dist(outgeom[-1][-1], geom[0][0])
-            d = _sqr_dist(outgeom[-1][-1], geom[-1][-1])
-            if d < mdist:
-                geom = [list(reversed(g)) for g in reversed(geom)]
-                mdist = d
-            # For the second way in the relation, we also allow the first
-            # to be turned, if the two ways aren't connected.
-            if is_turnable and len(outgeom) == 1:
-                d1 = _sqr_dist(outgeom[-1][0], geom[0][0])
-                d2 = _sqr_dist(outgeom[-1][0], geom[-1][-1])
-                if d1 < mdist or d2 < mdist:
-                    outgeom[-1].reverse()
-                if d2 < d1:
-                    geom = [list(reversed(g)) for g in reversed(geom)]
+        # try connect with previous geometry at end point
+        if outgeom.join_at_end(geom):
+            continue
 
-        outgeom.extend(geom)
-        is_turnable = True
+        # try to connect with previous geometry at start point
+        if outgeom.reverse_and_join_at_end(geom):
+            continue
 
-    if not outgeom:
-        return None
+        # nothing found, then turn the geometry such that the
+        # end points are as close together as possible
+        outgeom.add_at_distance(geom)
 
-    ret = LineString(outgeom[0]) if len(outgeom) == 1 else MultiLineString(outgeom)
 
-    return ret
-
+    return outgeom.to_shape()
---
+++ test/test_build_geometry.py
@@ -0,0 +1,81 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This file is part of Osgende.
+# Copyright (C) 2020 Sarah Hoffmann
+
+import unittest
+from unittest.mock import MagicMock
+from itertools import count
+
+from shapely.geometry import LineString, MultiLineString
+
+import osgende.common.build_geometry as builder
+
+class TestBuildGeometry(unittest.TestCase):
+
+    def build(self, *members):
+        rel_members = []
+        geoms = { 'W' : {}, 'R' : {} }
+        ids = { 'W' : count(1), 'R' : count(1) }
+        for member in members:
+            typ = member[0]
+            oid = next(ids[typ])
+
+            rel_members.append({'id' : oid, 'type' : typ, 'role' : ''})
+
+            nodes = [[(0.0, float(x)*0.0001) for x in s.split(',')]
+                     for s in member[1:].split(' ')]
+
+            if len(nodes) == 1:
+                geoms[typ][oid] = builder._MultiLine(LineString(nodes[0]))
+            else:
+                geoms[typ][oid] = builder._MultiLine(MultiLineString(nodes))
+
+        builder._get_member_geometries = MagicMock(return_value=geoms)
+        return builder.build_route_geometry(None, rel_members, None, None)
+
+    def check_geom(self, expected, result):
+        lines = expected.split()
+        if len(lines) == 1:
+            self.assertEqual('LineString', result.geom_type)
+            coords = [(0, float(c)*0.0001) for c in lines[0].split(',')]
+            self.assertListEqual(coords, list(result.coords))
+        else:
+            self.assertEqual('MultiLineString', result.geom_type)
+            self.assertEqual(len(lines), len(result.geoms))
+            for exp, res in zip(lines, result.geoms):
+                coords = [(0, float(c)*0.0001) for c in exp.split(',')]
+                self.assertListEqual(coords, list(res.coords))
+
+    def test_one_way(self):
+        self.check_geom('1,2,3', self.build('W1,2,3'))
+
+        self.check_geom('1,2,3', self.build('W1,2', 'W2,3'))
+        self.check_geom('1,2,3', self.build('W2,1', 'W2,3'))
+        self.check_geom('1,2,3', self.build('W1,2', 'W3,2'))
+        self.check_geom('1,2,3', self.build('W2,1', 'W3,2'))
+
+        self.check_geom('1,2 3,4', self.build('W1,2', 'W3,4'))
+        self.check_geom('1,2 3,4', self.build('W1,2', 'W4,3'))
+        self.check_geom('1,2 3,4', self.build('W2,1', 'W3,4'))
+        self.check_geom('1,2 3,4', self.build('W2,1', 'W4,3'))
+
+        self.check_geom('1,2 4,3,5', self.build('W1,2', 'W4,3', 'W3,5'))
+        self.check_geom('1,2 4,3,5', self.build('W1,2', 'W3,4', 'W3,5'))
+        self.check_geom('1,2 4,3,5', self.build('W1,2', 'W4,3', 'W5,3'))
+        self.check_geom('1,2 4,3,5', self.build('W1,2', 'W3,4', 'W5,3'))
+
+        self.check_geom('1,2,3,4', self.build('W2,1', 'W2,3', 'W3,4'))
+        self.check_geom('1,2,3,4', self.build('W2,1', 'W3,2', 'W3,4'))
+
+        self.check_geom('3,4 1,2', self.build('R3,4 1,2'))
+
+        self.check_geom('1,2 3,4,5 7,8', self.build('R1,2 3,4', 'R4,5 7,8'))
+        self.check_geom('1,2 3,4,5 7,8', self.build('R1,2 3,4', 'R8,7 5,4'))
+        self.check_geom('1,2 3,4,5 7,8', self.build('R4,3 2,1', 'R4,5 7,8'))
+        self.check_geom('1,2 3,4,5 7,8', self.build('R4,3 2,1', 'R8,7 5,4'))
+
+        self.check_geom('1,2 3,4 5,6 7,8', self.build('R1,2 3,4', 'R5,6 7,8'))
+        self.check_geom('1,2 3,4 5,6 7,8', self.build('R1,2 3,4', 'R8,7 6,5'))
+        self.check_geom('1,2 3,4 5,6 7,8', self.build('R4,3 2,1', 'R5,6 7,8'))
+        self.check_geom('1,2 3,4 5,6 7,8', self.build('R4,3 2,1', 'R8,7 6,5'))
