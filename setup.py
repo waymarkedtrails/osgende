9e561fc9ca0e1f354909d80a71ee630fef9d7e46
Sarah Hoffmann <lonvia@denofr.de>
2020-11-18 16:57:12 +0100

    make osgende installable again

--- setup.py
+++ setup.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python
-# This file is part of Osgende
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This file is part of osgende.
 # Copyright (C) 2012 Sarah Hoffmann
-#
-# This is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from distutils.core import setup
+
 setup(name='osgende',
-      version='1.0',
+      version='2.0',
       author='Sarah Hoffmann',
       author_email='lonvia@denofr.de',
-      url='https://github.com/lonvia/osgende',
+      url='https://github.com/waymarkedtrails/osgende',
       packages=['osgende',
                 'osgende.common',
                 'osgende.common.sqlalchemy',
                 'osgende.generic',
                 'osgende.lines',
-                'osgende.nodes',
                 'osgende.relations',
                ],
       scripts=['tools/osgende-import', 'tools/osgende-mapgen', 'tools/osgende-mapserv',
                'tools/osgende-mapserv-falcon.py'],
+      license='GPL 3.0',
+      keywords=["OSM", "OpenStreetMap", "Databases"],
+      python_requires = ">=3.7"
       )
