b98707f17c55bcea07a38a6166d168626452a879
Sarah Hoffmann <lonvia@denofr.de>
2020-11-28 22:16:03 +0100

    switch more tests to pytests

--- test/test_mapdb.py
+++ test/test_mapdb.py
@@ -3,40 +3,40 @@
 # This file is part of Osgende
 # Copyright (C) 2020 Sarah Hoffmann
 
-import unittest
+import pytest
 
 from osgende.mapdb import _Tables
 
-class TestTablesDict(unittest.TestCase):
+@pytest.fixture
+def tables():
+    tables = _Tables()
+    tables.add('foo', 23)
+    tables.add('bar', 45)
+    return tables
 
-    def setUp(self):
-        self.tables = _Tables()
-        self.tables.add('foo', 23)
-        self.tables.add('bar', 45)
+def test_get_as_dict(tables):
+    assert 23 == tables['foo']
+    assert 45 == tables['bar']
 
-    def test_get_as_dict(self):
-        self.assertEqual(23, self.tables['foo'])
-        self.assertEqual(45, self.tables['bar'])
+def test_get_as_dict_fail(tables):
+    with pytest.raises(KeyError):
+        tables['baz']
 
-    def test_get_as_dict_fail(self):
-        with self.assertRaises(KeyError):
-            self.tables['baz']
+def test_in(tables):
+    assert 'foo' in tables
+    assert 'nothing' not in tables
 
-    def test_in(self):
-        self.assertTrue('foo' in self.tables)
-        self.assertFalse('nothing' in self.tables)
+def test_get_as_attribute(tables):
+    23 == tables.foo
+    45 == tables.bar
 
-    def test_get_as_attribute(self):
-        self.assertEqual(23, self.tables.foo)
-        self.assertEqual(45, self.tables.bar)
+def test_get_as_attribute_fail(tables):
+    with pytest.raises(AttributeError):
+        tables.baz
 
-    def test_get_as_attribute_fail(self):
-        with self.assertRaises(AttributeError):
-            self.tables.baz
+def test_has_attribute(tables):
+    assert hasattr(tables, 'foo')
+    assert not hasattr(tables, 'foot')
 
-    def test_has_attribute(self):
-        self.assertTrue(hasattr(self.tables, 'foo'))
-        self.assertFalse(hasattr(self.tables, 'foot'))
-
-    def test_len(self):
-        self.assertEqual(2, len(self.tables))
+def test_len(tables):
+    assert 2 == len(tables)
