80a18493a9767fb2ee16dfa1f18ed2bf57d07151
Sarah Hoffmann <lonvia@denofr.de>
2022-01-04 16:09:14 +0100

    port thread tests to pytest

--- test/test_threads.py
+++ test/test_threads.py
@@ -1,64 +1,58 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 # This file is part of Osgende
-# Copyright (C) 2020 Sarah Hoffmann
+# Copyright (C) 2022 Sarah Hoffmann
 """ Test threading helpers.
 """
-
-import unittest
 import time
 from itertools import count
 
+import pytest
+
 from osgende.common.threads import WorkerQueue, WorkerError
 
-class TestWorkerQueue:
+@pytest.mark.parametrize('threads', (0, 3))
+def test_init_func(threads):
+    init_called = count()
 
-    def test_init_func(self):
-        init_called = count()
+    queue = WorkerQueue(lambda x: x, numthreads=threads,
+                        initfunc=init_called.__next__)
 
-        queue = WorkerQueue(lambda x: x, numthreads=self.numthreads,
-                            initfunc=init_called.__next__)
+    queue.finish()
 
-        queue.finish()
-
-        self.assertEqual(max(1, self.numthreads), next(init_called))
-
-    def test_add_task(self):
-        done = set()
-
-        queue = WorkerQueue(done.add, numthreads=self.numthreads)
-
-        for i in range(100):
-            queue.add_task(i)
-
-        queue.finish()
-
-        self.assertSetEqual(set(range(100)), done)
-
-    def test_shutdown_func(self):
-        shutdown_called = count()
-
-        queue = WorkerQueue(lambda x: x, numthreads=self.numthreads,
-                            shutdownfunc=shutdown_called.__next__)
-
-        queue.finish()
-
-        self.assertEqual(max(1, self.numthreads), next(shutdown_called))
+    assert max(1, threads) == next(init_called)
 
 
+@pytest.mark.parametrize('threads', (0, 3))
+def test_add_task(threads):
+    done = set()
+
+    queue = WorkerQueue(done.add, numthreads=threads)
+
+    for i in range(100):
+        queue.add_task(i)
+
+    queue.finish()
+
+    assert set(range(100)) == done
 
 
-class TestSimpleWorkerQueue(TestWorkerQueue, unittest.TestCase):
-    numthreads = 0
+@pytest.mark.parametrize('threads', (0, 3))
+def test_shutdown_func(threads):
+    shutdown_called = count()
 
-class TestThreadsWorkerQueue(TestWorkerQueue, unittest.TestCase):
-    numthreads = 3
+    queue = WorkerQueue(lambda x: x, numthreads=threads,
+                        shutdownfunc=shutdown_called.__next__)
 
-    def test_queue_deadlock(self):
-        queue = WorkerQueue(time.sleep, numthreads=1)
+    queue.finish()
 
-        queue.add_task('x')
-        with self.assertRaises(WorkerError):
-            for i in range(1000):
-                queue.add_task(0.1)
+    assert max(1, threads) == next(shutdown_called)
 
+
+def test_queue_deadlock():
+    queue = WorkerQueue(time.sleep, numthreads=1)
+
+    queue.add_task('x')
+    with pytest.raises(WorkerError):
+        for i in range(1000):
+            queue.add_task(0.1)
